b06a72f46ab199355d5f94a6be92cf35b1de69ab
Kshitija Pagare <pagarekshitija17@gmail.com>
2022-10-22 14:23:57 +0530

    first commit

---
+++ task1/index.js
@@ -0,0 +1,10 @@
+// program to display text 5 times
+const n = 10;
+
+// looping from i = 1 to 5
+for (let i = 1; i <= n; i++) {
+    console.log(`At Floor: `  + i);
+}
+
+var test = "Hello";
+document.getElementById("ivalue").value = i;
